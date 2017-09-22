607eba594e2a37fc0231e922d98034706466e66c
Kis-Szabo Norbert <norbert.kis-szabo@oce.com>
2017-09-22 17:11:07 +0200

    (deeplearning/udemy): ann tutorial

--- deeplearningspace/Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/my_ann.py
+++ deeplearningspace/Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/my_ann.py
@@ -13,9 +13,10 @@ Y = data.iloc[:, -1:].values
 # Create dummies
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-x_encoder = LabelEncoder()
-X[:, 1] = x_encoder.fit_transform(X[:, 1])
-X[:, 2] = x_encoder.fit_transform(X[:, 2])
+x_encoder1 = LabelEncoder()
+X[:, 1] = x_encoder1.fit_transform(X[:, 1])
+x_encoder2 = LabelEncoder()
+X[:, 2] = x_encoder2.fit_transform(X[:, 2])
 
 onehot = OneHotEncoder(categorical_features=[1])
 
@@ -48,4 +49,24 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 classifier = Sequential()
-classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11)))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(11,)))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_shape=(6,)))
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid', input_shape=(6,) ))
+
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'], )
+
+classifier.fit(X_tr, y_tr, batch_size = 10, epochs = 100)
+
+y_pred = classifier.predict(X_t)
+y_pred = np.round(y_pred)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_t, y_pred)
+
+hw = np.array([[600, 'France', 'Male', 40, 3, 60000, 2, 1, 1, 500000 ]])
+
+hw[:,1] = x_encoder1.transform(hw[:,1])
+hw[:,2] = x_encoder2.transform(hw[:,2])
+hw = onehot.transform(hw).toarray()
+
+hw = hw[:, 1:]
+classifier.predict(x_scale.transform(hw))
